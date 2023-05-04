5199cad7da402e82551e3f01701ed4b8c76c3252
Evgenija123 <evgenija.divitarova1@gmail.com>
2023-05-04 21:01:53 +0200

    Dodadeni components

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            counter:0,
+           
+        };
+        this.increment=this.increment.bind(this);
+        this.decrement=this.decrement.bind(this);
+    }
+    increment(){
+       
+        this.setState({
+            counter:this.state.counter+1
+        } );
+    
+    }
+
+    decrement(){
+       
+        this.setState({
+            counter:this.state.counter-1
+        } );
+    
+    }
+        
+        
+
+render(){
+    return (<div>
+    <h3>Counter value is:{this.state.counter}</h3>
+    <button onClick={this.increment}>Zgolemi</button>
+    <button onClick={this.decrement}>Namali</button>
+    </div>)
+}
+}
+export default Counter;
---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+function Hello(){
+    return<h1>Hello World! Evgenija</h1>
+
+}
+
+
+export default Hello;
+
+/*
+
+const Hello=()=><h1>Hello There</h1>
+*/
